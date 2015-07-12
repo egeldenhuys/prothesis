1c0c8c74cea066e087ea4f484a32ba3149e8d018
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2015-07-12 21:54:06 +0200

    Create GUI front end

--- src/prothesis-gui/mainwindow.cpp
+++ src/prothesis-gui/mainwindow.cpp
@@ -3,8 +3,8 @@
 #include <QString>
 #include <QLineEdit>
 
-#include "../user.h"
-#include "../globals.h"
+//#include "../user.h"
+//#include "../globals.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -18,15 +18,3 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_btnLoad_clicked()
-{
-    UserData.load();
-    ui->txtName->setText(QString::fromStdString(UserData.name));
-}
-
-void MainWindow::on_btnSave_clicked()
-{
-    QString name = ui->txtName->text();
-    UserData.name = name.toUtf8().constData();
-    UserData.save();
-}
