59058d0ccac9fde7241a969a1d2399492e9ea42b
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-03-07 13:52:21 +0200

    Change deploy directory structure

--- deploy/deploy.cmd
+++ deploy/deploy.cmd
@@ -1,14 +1,15 @@
 @ECHO OFF
 
 REM TODO: qmake
-cd ..\src\build-prothesis-gui-Desktop_Qt_5_5_0_MinGW_32bit-Release\release
-COPY prothesis-gui.exe ..\..\..\deploy\prothesis_files\
-cd ..\..\..\deploy\prothesis_files\
+mkdir prothesis-release\prothesis_files\
+
+COPY ..\src\build-prothesis-gui-Desktop_Qt_5_5_0_MinGW_32bit-Release\release\prothesis-gui.exe prothesis-release\prothesis_files\
 
 REM TODO: Get system var instead of hard link
+cd prothesis-release\prothesis_files\
 E:\Qt\Qt5.5.0\5.5\mingw492_32\bin\windeployqt --release --force --compiler-runtime prothesis-gui.exe
 COPY E:\Qt\Qt5.5.0\5.5\mingw492_32\bin\libgcc_s_dw2-1.dll .
 COPY E:\Qt\Qt5.5.0\5.5\mingw492_32\bin\libwinpthread-1.dll .
 COPY "E:\Qt\Qt5.5.0\5.5\mingw492_32\bin\libstdc++-6.dll" .
 
-cd ..
+cd ..\..\
