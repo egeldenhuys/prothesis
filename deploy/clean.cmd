bab0e030df53efac661b6d5e460e3857543116f5
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-04-27 22:57:54 +0200

    Update change log

--- deploy/clean.cmd
+++ deploy/clean.cmd
@@ -1,1 +1,2 @@
-rmdir /S /Q prothesis-release\prothesis_files
+RMDIR /S /Q prothesis-release\prothesis_files
+DEL prothesis-release\CHANGELOG.md
--- deploy/deploy.cmd
+++ deploy/deploy.cmd
@@ -5,6 +5,8 @@ mkdir prothesis-release\prothesis_files\
 
 COPY ..\src\build-prothesis-gui-Desktop_Qt_5_5_0_MinGW_32bit-Release\release\prothesis-gui.exe prothesis-release\prothesis_files\
 
+COPY ..\CHANGELOG.md prothesis-release\
+
 REM TODO: Get system var instead of hard link
 cd prothesis-release\prothesis_files\
 E:\Qt\Qt5.5.0\5.5\mingw492_32\bin\windeployqt --release --force --compiler-runtime prothesis-gui.exe
